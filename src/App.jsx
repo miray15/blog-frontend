b3617ac43d9e32d2e3a68edeaa2c2f3440b522ec
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-13 12:38:04 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+
+      <div id="posts-new">
+        <h1>New post</h1>
+
+        
+      </div>
+
+      <div id="posts-index">
+        <h1>All posts</h1>
+      </div>
+
+      <footer>
+        <p>Copyright 2023</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
