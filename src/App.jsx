d221797c31d15832fa76e9317cfd2617f76802bd
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-14 15:53:52 -0500

    add axios gem

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,24 @@ function Content() {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      title: "Raw Eggs",
-      chef: "Peter Jang",
-      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
+      title: "Hello",
+      body: "body",
+      image: "image1",
     },
     {
       id: 2,
-      title: "Mud Pie",
-      chef: "Jay Wengrow",
-      image_url:
-        "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
+      title: "hi there",
+      body: "body body",
+      image: "image2",
     },
     {
       id: 3,
-      title: "Pizza!!!!",
-      chef: "Jay Wengrow",
-      image_url:
-        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
+      title: "good day",
+      body: "body body body",
+      image: "image3",
     },
-  ])
+  ]);
+
     
   
   return(
