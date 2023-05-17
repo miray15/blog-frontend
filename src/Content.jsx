f02bfba4d5fc5275e1bf4c141830d458541ad699
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-17 13:36:09 -0500

    add synax to Content and BlogsNew

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,41 +1,59 @@
-import { Signup } from "./Signup";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { PostsNew } from "./PostsNew";
 import { PostsIndex } from "./PostsIndex";
-import { useState } from "react";
+// import { PostsShow } from "./PostsShow";
+// import { Modal } from "./Modal";
+import { Signup } from "./Signup";
 import { Login } from "./Login";
-import { LogoutLink } from "./LogoutLink";
-import { axios } from "axios";
+import { LogoutLink } from "./LogoutLink"; //
 
 export function Content() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Hello",
-      body: "body",
-      image: "image1",
-    },
-    {
-      id: 2,
-      title: "hi there",
-      body: "body body",
-      image: "image2",
-    },
-    {
-      id: 3,
-      title: "good day",
-      body: "body body body",
-      image: "image3",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
+  const [currentPost setCurrentPost] = useState({});
+  const handleIndexPosts = () => {
+  
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      
+      setPosts(response.data);
+    });
+  };
+  useEffect(handleIndexPosts, []);
+  const handleShowPost = (myPost) => {
+    console.log(myPost);
+    console.log(
+      "handling show Post.eventually this will change the value of isPostsShowVisible and trigger the modal"
+    );
+    setIsPostsShowVisible(true);
+    setCurrentPost(myPost);
+  };
+
+  const handleClose = () => {
+    console.log("close modal");
+    setIsPostsShowVisible(false);
+  };
+
+  const handleCreatePost = (params) => {
+    axios
+      .post("http://localhost:3000/posts.json", params)
+      .then((response) => {
+        console.log(response.data);
+  
+        setPosts([...posts, response.data]);
+      });
+    console.log("handling create post");
+  };
 
   return (
     <div className="container">
       <Signup />
       <Login />
       <LogoutLink />
-      <PostsNew />
+      <PostsNew onCreatePost={handleCreatePost} />
       <PostsIndex post={posts} />
-
+      <br />
       <br />
       <br />
     </div>
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,18 +1,33 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handling submit");
+    // console.log(event)
+    // // make web request to axios
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    });
+    // send the params
+    // clear the form
+  };
   return (
     <div>
       <div id="posts-new">
         <h1>New post</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            Title: <input type="text" />
+            Title: <input name="title" type="text" />
           </div>
           <div>
-            Body: <input type="text" />
+            Body: <input name="body" type="text" />
           </div>
           <div>
-            Image: <input type="text" />
+            Image: <input name="image" type="text" />
           </div>
           <button type="Submit">Create Post</button>
         </form>
